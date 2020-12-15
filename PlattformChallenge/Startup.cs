9011208cef36400f0afc50486b3500d80dba202c
Zhen Fan <ubumh@student.kit.edu>
2020-12-15 01:36:44 +0100

    set the StartUp

--- PlattformChallenge/Startup.cs
+++ PlattformChallenge/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using PlattformChallenge.Models;
 
 namespace PlattformChallenge
 {
@@ -23,6 +26,10 @@ namespace PlattformChallenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("ChallengeDBConnection"))
+            );
+
             services.AddControllersWithViews();
         }
 
