5a5f572a14523743eda43ec4647b4c8205fc4b85
Zhicheng Xiang <ukfdc@student.kit.edu>
2020-12-19 23:34:42 +0800

    create test project

---
+++ PlattformChallengeTests1/Controllers/AccountControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlattformChallenge.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlattformChallenge.Controllers.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        [TestMethod()]
+        public void LogoutTest()
+        {
+
+        }
+    }
+}
